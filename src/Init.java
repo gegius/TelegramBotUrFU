62de8f744b1ca53afe83617ce681003789ba54a7
gegius <glebushka.ageev@gmail.com>
2020-11-15 22:06:15 +0500

    Там внутри комменты - почитай. По сути я создал возможность менять кнопки на определенные команды. Запрос: придумать декомпозицию.

--- src/Init.java
+++ src/Init.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -42,48 +43,93 @@ public class Init extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
-            // Set variables
-            String message_text = "Привет! Я бот для игры в RPG. Если хочешь начать - нажми продолжить или начать новую игру!";
-            long chat_id = update.getMessage().getChatId();
-
-            SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(message_text);
-            try {
-                execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+        String message = update.getMessage().getText();
+        if(message.equals("/create_hero")) {
+            sendMsg(update.getMessage().getChatId().toString(), "Выберите класс!", "/create_hero");
+        }
+        if(message.equals("/start")) {
+            sendMsg(update.getMessage().getChatId().toString(), "Здравствуйте, это бот для РПГ игры, пожалуйста," +
+                    "продолжите свою игру или начните сначала", "/start");
+        }
+        if(message.equals("Воин")) {
+            sendMsg(update.getMessage().getChatId().toString(), "Ваш класс Воин!", "Воин");
         }
     }
 
-    public synchronized void setButtons(SendMessage sendMessage) {
-        // Создаем клавиуатуру
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+
+    public synchronized void sendMsg(String chatId, String s, String command) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+
+        //смотрим что ответил наш чел и даем ему соответствующие кнопки
+
+        if(command.equals("/create_hero")) {
+          createHeroBut(sendMessage, "Маг", "Воин");
+        }
+        if(command.equals("/start")) {
+            createHeroBut(sendMessage, "Продолжить", "/create_hero");
+        }
+        if(command.equals("Воин")){
+            createHeroBut(sendMessage, "Задать статы", "Задать описание");
+        }
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public synchronized void createHeroBut(SendMessage sendMessage, String butName1, String butName2) {
+
+// Создаем клавиуатуру
+
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
 
-        // Создаем список строк клавиатуры
+
+// Создаем список строк клавиатуры
+
         List<KeyboardRow> keyboard = new ArrayList<>();
 
-        // Первая строчка клавиатуры
+
+// Первая строчка клавиатуры
+
         KeyboardRow keyboardFirstRow = new KeyboardRow();
-        // Добавляем кнопки в первую строчку клавиатуры
-        keyboardFirstRow.add(new KeyboardButton("Начать заново"));
-        keyboardFirstRow.add(new KeyboardButton("Продлжить игру"));
-        // Вторая строчка клавиатуры
-        //KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+// Добавляем кнопки в первую строчку клавиатуры и задаем название
+
+        keyboardFirstRow.add(new KeyboardButton(butName1));
 
 
-        // Добавляем все строчки клавиатуры в список
+// Вторая строчка клавиатуры
+
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+// Добавляем кнопки во вторую строчку клавиатуры и зажаем название
+
+        keyboardSecondRow.add(new KeyboardButton(butName2));
+
+
+// Добавляем все строчки клавиатуры в список
+
         keyboard.add(keyboardFirstRow);
-        //keyboard.add(keyboardSecondRow);
-        // и устанваливаем этот список нашей клавиатуре
+        keyboard.add(keyboardSecondRow);
+
+// и устанваливаем этот список нашей клавиатуре
+
         replyKeyboardMarkup.setKeyboard(keyboard);
     }
+
 }
