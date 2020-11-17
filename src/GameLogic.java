fa85ea04c005403fdc76d18e36a685c2a0bf4dc8
gegius <glebushka.ageev@gmail.com>
2020-11-17 10:07:15 +0500

    Реализовано UI создания класса мага, решена (частично) проблема с возможностью написав класс его выбрать в любое время

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -9,17 +9,27 @@ import java.util.List;
 
 public class GameLogic extends Init{
 
+    public static String HeroClass = "";
+
     public void Game(String command, String chatId){
 
         if(command.equals("/create_hero")) {
             sendMsg(chatId, "Выберите класс!", "/create_hero");
+            HeroClass = "";
         }
         if(command.equals("/start")) {
             sendMsg(chatId, "Здравствуйте, это бот для РПГ игры, пожалуйста," +
                     "продолжите свою игру или начните сначала", "/start");
         }
         if(command.equals("Воин")) {
-            sendMsg(chatId, "Ваш класс Воин!", "Воин");
+            if(HeroClass.equals("")){
+                sendMsg(chatId, "Ваш класс Воин!", "Воин");
+            }
+        }
+        if(command.equals("Маг")) {
+            if(HeroClass.equals("")){
+                sendMsg(chatId, "Ваш класс Маг!", "Маг");
+            }
         }
     }
 
@@ -38,6 +48,7 @@ public class GameLogic extends Init{
         //смотрим что ответил наш чел и даем ему соответствующие кнопки
 
         ButtonBuilder Buttons = new ButtonBuilder();
+        List<String> mainMenu = Arrays.asList("Inventory", "Stats", "Leave", "Help");
 
 
         if(command.equals("/create_hero")) {
@@ -49,8 +60,12 @@ public class GameLogic extends Init{
             Buttons.createHeroBut(sendMessage, buttons);
         }
         if(command.equals("Воин")){
-            List<String> buttons = Arrays.asList("Stats", "Info");
-            Buttons.createHeroBut(sendMessage, buttons);
+            Buttons.createHeroBut(sendMessage, mainMenu);
+            HeroClass = "Warrior";
+        }
+        if(command.equals("Маг")){
+            Buttons.createHeroBut(sendMessage, mainMenu);
+            HeroClass = "Mage";
         }
         try {
             execute(sendMessage);
